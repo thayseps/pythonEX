d0c387e2b8bc171b6238c9ac55805563643e6b18
Thayse <78800993+thayseps@users.noreply.github.com>
2023-03-10 20:28:02 -0300

    create primos.py

---
+++ primos.py
@@ -0,0 +1,16 @@
+num = int(input('Digite um número: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m', end=' ')
+        tot += 1
+    else:
+        print('\033[31m', end=' ')
+    print('{} '.format(c),end=' ' )
+
+print('\033[m \nO número {} foi dividido {} vezes'.format(num, tot))
+
+if tot == 2:
+    print('E por isso ele é primo!')
+else:
+    print('E por isso ele NÃO é primo!')
